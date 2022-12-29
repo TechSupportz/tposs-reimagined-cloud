38986dc062bd52f971eec35c98fc78c7c3cdcee0
Nitish <tnitish654@gmail.com>
2022-12-29 20:38:59 +0800

    ⚡ Organised lambdas in folders
    ✨ Get SEAL Records By Type Lambda works

---
+++ aws/lambda/SEAL Points/getSEALRecordsByType/index.mjs
@@ -0,0 +1,72 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb"
+import { DynamoDBDocumentClient, ExecuteStatementCommand } from "@aws-sdk/lib-dynamodb"
+
+const REGION = "us-east-1"
+const headers = { "Content-Type": "application/json" }
+
+const dynamodb = new DynamoDBClient({ region: REGION })
+const ddbDocClient = DynamoDBDocumentClient.from(dynamodb)
+
+export const handler = async (event) => {
+	const { studentId, type } = event.pathParameters
+
+	console.log("studentId: " + studentId)
+	console.log("type: " + type)
+
+	if (
+		type !== "Service" &&
+		type !== "Enrichment" &&
+		type !== "Achievement" &&
+		type !== "Leadership"
+	) {
+		return {
+			statusCode: 400,
+			headers: headers,
+			body: JSON.stringify({
+				message: "Invalid SEAL type provided",
+			}),
+		}
+	}
+
+	const executeStatementCommand = new ExecuteStatementCommand({
+		Statement: `SELECT * FROM SEALPoints WHERE student_id = ? AND type = ?`,
+		Parameters: [studentId, type],
+	})
+
+	try {
+		const data = await ddbDocClient.send(executeStatementCommand)
+		console.log(data)
+
+		return {
+			statusCode: 200,
+			headers: headers,
+			body: JSON.stringify({
+				message: "Successfully retrieved SEAL records",
+				items: data.Items,
+			}),
+		}
+	} catch (err) {
+		console.log(err)
+		return {
+			statusCode: 500,
+			headers: headers,
+			body: JSON.stringify({
+				message: err,
+			}),
+		}
+	}
+}
+
+/* 
+cSpell:disable 
+This is just for local testing purposes
+Ensure this is commented out when deploying to AWS
+*/
+
+// console.log("Running locally")
+// handler({
+// 	pathParameters: {
+// 		studentId: "2101220J",
+// 		type: "Leadershi",
+// 	},
+// })
