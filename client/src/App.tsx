1c76f71f5022e07268b3b0216a5f702262678c15
Nitish <79391172+TechSupportz@users.noreply.github.com>
2023-01-28 10:46:24 +0800

    Initial setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,111 +1,26 @@
-import axios from "axios"
-import React, { useEffect } from "react"
+import {
+    AppShell,
+    Center,
+    Header,
+    Navbar,
+    Text,
+    Title,
+    Stack,
+    Button,
+} from "@mantine/core"
 
-export interface cognitoTokens {
-	access_token: string
-	expires_in: number
-	id_token: string
-	refresh_token: string
-	token_type: string
-}
+import React from "react"
 
 const App = () => {
-	const [authCode, setAuthCode] = React.useState<string | null>(null)
-	const [tokens, setTokens] = React.useState<cognitoTokens | null>(null)
-
-	useEffect(() => {
-		const url = window.location.href
-
-		setAuthCode(url.split("code=")[1])
-	}, [])
-
-	useEffect(() => {
-		if (authCode) {
-			console.log(authCode)
-			fetchTokens(authCode)
-		}
-	}, [authCode])
-
-	const fetchTokens = async (authCode: string) => {
-		try {
-			const res = await fetch(
-				"https://tposs-reimagined.auth.us-east-1.amazoncognito.com/oauth2/token",
-				{
-					method: "POST",
-					headers: {
-						"Content-Type": "application/x-www-form-urlencoded",
-					},
-					body: new URLSearchParams({
-						grant_type: "authorization_code",
-						code: authCode,
-						client_id: "4iermftj0fjo513022qtg0n464",
-						redirect_uri: "https://main.d3cvr0pboc0tkg.amplifyapp.com/",
-						// redirect_uri: "http://localhost:3000",
-					}),
-					mode: "cors",
-				}
-			)
-			const data = await res.json()
-			console.log(data)
-			setTokens(data)
-		} catch (err) {
-			console.error(err)
-		}
-	}
-
-	const copyToClipboard = (text: string) => {
-		navigator.clipboard.writeText(text)
-	}
-
-	return (
-		<>
-			<h1>TPOSS: Reimagined</h1>
-			<h2>JWT Tokens</h2>
-			<a href="https://tposs-reimagined.auth.us-east-1.amazoncognito.com/logout?client_id=4iermftj0fjo513022qtg0n464&redirect_uri=https://main.d3cvr0pboc0tkg.amplifyapp.com/&response_type=code">
-				<button>Logout</button>
-			</a>
-
-			<div className="token">
-				<h3>ID Token (JWT for authorization)</h3>
-				{tokens ? (
-					<button onClick={() => copyToClipboard(tokens.id_token)}>Copy</button>
-				) : (
-					<></>
-				)}
-				<p>{tokens?.id_token}</p>
-			</div>
-
-			<div className="token">
-				<h3>Access Token</h3>
-				{tokens ? (
-					<button onClick={() => copyToClipboard(tokens.access_token)}>Copy</button>
-				) : (
-					<></>
-				)}
-				<p>{tokens?.access_token}</p>
-			</div>
-
-			<div className="token">
-				<h3>Refresh Token</h3>
-				{tokens ? (
-					<button onClick={() => copyToClipboard(tokens.refresh_token)}>Copy</button>
-				) : (
-					<></>
-				)}
-				<p>{tokens?.refresh_token}</p>
-			</div>
-
-			<div className="token">
-				<h3>Token Type</h3>
-				<p>{tokens?.token_type}</p>
-			</div>
-
-			<div className="token">
-				<h3>Expires In</h3>
-				<p>{tokens?.expires_in}</p>
-			</div>
-		</>
-	)
+    return (
+        <Center h="100vh">
+            <Stack align="center">
+                <Title>Title - Raleway</Title>
+                <Text>Hello - Lato</Text>
+                <Button>Button!</Button>
+            </Stack>
+        </Center>
+    )
 }
 
 export default App
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,5 +2,32 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
+import { MantineProvider } from "@mantine/core"
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+            fontFamily: "Lato, sans-serif",
+            headings: { fontFamily: "Raleway, sans-serif" },
+            colors: {
+                brand: [
+                    "#ffe5eb",
+                    "#fcb7c5",
+                    "#f58a9b",
+                    "#ef5c6e",
+                    "#e93052",
+                    "#d01845",
+                    "#a3103f",
+                    "#750934",
+                    "#480424",
+                    "#1e000f",
+                ],
+            },
+            primaryColor: "brand",
+            primaryShade: 3,
+        }}>
+        <App />
+    </MantineProvider>,
+)
